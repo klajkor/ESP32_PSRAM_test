4de94d36926805f53ce64719f15899d92c0c1da8
Robert Klajko <robert_klajko@epam.com>
2021-05-13 23:28:18 +0200

    Reformatted again

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #define GPIO_LED1 2
 #define GPIO_LED2 5
 
-void setup() {
+void setup()
+{
     pinMode(GPIO_LED1, OUTPUT);
     pinMode(GPIO_LED2, OUTPUT);
     digitalWrite(GPIO_LED1, HIGH);
@@ -18,17 +19,18 @@ void setup() {
     esp_chip_info(&chip_info);
     //get chip id
     uint64_t chipid;
-    chipid=ESP.getEfuseMac();
+    chipid = ESP.getEfuseMac();
 
     log_d("Hardware info");
     log_d("%d cores Wifi %s%s", chip_info.cores, (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "", (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
     log_d("Silicon revision: %d", chip_info.revision);
     log_d("ESP32 Chip model: %s Rev %d", ESP.getChipModel(), ESP.getChipRevision());
-    log_d("ESP32 Chip ID: %04X%08X\n",(uint16_t)(chipid>>32),(uint32_t)chipid);
-    log_d("%dMB %s flash", spi_flash_get_chip_size()/(1024*1024), (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embeded" : "external");
+    log_d("ESP32 Chip ID: %04X%08X\n", (uint16_t)(chipid >> 32), (uint32_t)chipid);
+    log_d("%dMB %s flash", spi_flash_get_chip_size() / (1024 * 1024), (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embeded" : "external");
 }
 
-void loop() {
+void loop()
+{
     // put your main code here, to run repeatedly:
     digitalWrite(GPIO_LED1, HIGH);
     digitalWrite(GPIO_LED2, HIGH);
